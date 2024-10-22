b5330748b9b7271140be127674b51c045d655b1e
Tsan Tri Duc <tsantriduc@gmail.com>
2024-10-23 02:41:27 +0700

    set up file and store redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,7 @@
-import logo from './logo.svg'
+import Blog from 'pages/blog'
 
 function App() {
-  return (
-    <div className='App'>
-
-      <a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
-        Learn React
-      </a>
-
-    </div>
-  )
+  return <Blog />
 }
 
 export default App
